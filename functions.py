e72bb5780c0967fa70672fe10a003194064a1c0a
Quinter-Jaika <139950430+Quinter-Jaika@users.noreply.github.com>
2024-05-14 16:45:03 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,11 +1,3 @@
 # Generate email addresses
-    def generate_email(name):
-        # Split student name into first name and last name
-        parts = name.split()
-        first_name = parts[0]
-        last_name = parts[-1] if len(parts) > 1 else ""
-        # Remove special characters such as ' from last name using regular expressions
-        last_name = re.sub(r'[^a-zA-Z]', '', last_name)
-        # Generate email address
-        email = f"{first_name[0]}{last_name}@gmail.com".lower()
-        return email
+# Generate gender list and count
+# Save
